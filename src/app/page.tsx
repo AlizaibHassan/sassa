3c318ab3eeae6ed6f0fd2fa03dc5fe95a95ce490
Alizaib Hassan <AlizaibHassan@gmail.com>
2025-04-23 12:58:14 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -63,6 +63,16 @@ export default async function Home() {
             <h1 className="text-4xl sm:text-5xl font-bold text-gray-900 mb-6">
               SASSA Status Check
             </h1>
+            
+            <ins
+              className="adsbygoogle"
+              style={{ display: 'block' }}
+              data-ad-client="ca-pub-5087645624131694"
+              data-ad-slot="5558342186"
+              data-ad-format="auto"
+              data-full-width-responsive="true"
+            ></ins>
+            
           </div>
         </div>
         
